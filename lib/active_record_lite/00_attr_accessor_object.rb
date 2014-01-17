2aa185210871b9d60ada1f93f2dc46dc96724d82
= <=>
2014-01-17 12:59:08 -0500

    Adds 00 AttrAccessorObject::my_attr_accessor.

--- lib/active_record_lite/00_attr_accessor_object.rb
+++ lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+
+    names.each do |name|
+      define_method(name) do
+        self.instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |arg|
+        self.instance_variable_set("@#{name}".to_sym, arg)
+      end
+    end
   end
 end
