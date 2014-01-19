abc18a28aed2d01aff7e1e8389dcbd4f4e6fc34e
R <student@appacademy.io>
2014-01-19 16:55:39 -0500

    Adds ::has_one_through method to Associatable Module.

--- lib/active_record_lite/05_associatable2.rb
+++ lib/active_record_lite/05_associatable2.rb
@@ -1,10 +1,38 @@
 require_relative '04_associatable'
 
-# Phase V
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    # self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_pkey = through_options.primary_key
+      through_fkey = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_pkey = source_options.primary_key
+      source_fkey = source_options.foreign_key
+
+
+      key_val = self.send(through_fkey)
+      results = DBConnection.execute(<<-SQL, key_val)
+      SELECT
+        #{source_table}.*
+      FROM
+        #{through_table}
+      JOIN
+        #{source_table}
+      ON
+        #{source_table}.#{source_pkey} = #{through_table}.#{source_fkey}
+      WHERE
+        #{through_table}.#{through_pkey} = ?
+      SQL
+
+
+      source_options.model_class.parse_all(results).first
+    end
   end
 end
